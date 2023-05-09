722e648e2dde0b5316d8aa0705454af583326f5d
AldoLunaBueno <alunab@uni.pe>
2023-05-08 21:08:55 -0500

    Refactoring tests

--- Arrays-and-hashing/test_contains_duplicate.py
+++ Arrays-and-hashing/test_contains_duplicate.py
@@ -1,20 +1,30 @@
 import unittest
 from contains_duplicate import *
 
+
 class Test(unittest.TestCase):
     def test_contains_duplicate(self):
-        list1 = [1, 1]
-        list2 = [1,2,3,1]
-        list3 = [1,1,1,3,3,4,3,2,4,2]
-        self.assertTrue(Solution.containsDuplicate(self, list1))
-        self.assertTrue(Solution.containsDuplicate(self, list2))
-        self.assertTrue(Solution.containsDuplicate(self, list3))
+        lists = [
+            [1, 1],
+            [1, 2, 3, 1],
+            [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+        ]
+        Test.assertContainsDuplicate(self, lists, True)
+
     def test_contains_distinct(self):
-        list00 = []
-        list0 = [1]
-        list1 = [1, 2]
-        list2 = [1,2,3,4]
+        lists = [
+            [],
+            [1],
+            [1, 2],
+            [1, 2, 3, 4]
+        ]
+        Test.assertContainsDuplicate(self, lists, False)
+        """
+        Refactorized:
         self.assertFalse(Solution.containsDuplicate(self, list00))
         self.assertFalse(Solution.containsDuplicate(self, list0))
-        self.assertFalse(Solution.containsDuplicate(self, list1))
-        self.assertFalse(Solution.containsDuplicate(self, list2))
+        ...
+        """
+    def assertContainsDuplicate(self, lists, boolean):
+        for nums in lists:
+            self.assertEqual(Solution.containsDuplicate(self, nums), boolean)
