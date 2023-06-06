e2a34a6bda7b2d737068872be68a97338bbb36f3
AldoLunaBueno <alunab@uni.pe>
2023-06-06 10:12:50 -0500

    3Sum Test Update

--- Two-pointers/3Sum/test_3sum.py
+++ Two-pointers/3Sum/test_3sum.py
@@ -11,7 +11,13 @@ class Test:
         ([0,0,0], [[0,0,0]])
     ])
     def test_cases(self, input, expected):
-        assert dd(sol.threeSum(input), expected, ignore_order=True) == {}
+        result = sol.threeSum(input)
+        if dd(result, expected, ignore_order=True) != {}:
+            info = f'''Lists are not equal ignoring order.
+            result =   {result}
+            expected = {expected}
+                                    '''
+            assert False, info
 
 
 if __name__=="__main__":
