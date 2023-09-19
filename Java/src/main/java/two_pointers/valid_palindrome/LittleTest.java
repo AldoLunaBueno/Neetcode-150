a509235261673a2e55ae9e3d887408d5c2942b7e
AldoLunaBueno <alunab@uni.pe>
2023-09-19 14:11:27 -0500

    Done Java valid palindrome

---
+++ Java/src/main/java/two_pointers/valid_palindrome/LittleTest.java
@@ -0,0 +1,10 @@
+package two_pointers.valid_palindrome;
+
+public class Test {
+    public static void main(String[] args) {
+        char c = '0';
+        System.out.println(
+                Character.isLowerCase(c)
+        );
+    }
+}
---
+++ Java/src/main/java/two_pointers/valid_palindrome/Solution.java
@@ -0,0 +1,8 @@
+package two_pointers.valid_palindrome;
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+        while
+        return false;
+    }
+}
